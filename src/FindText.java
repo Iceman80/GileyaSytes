de6269fcc5a8a6d44712b8f6508376ce987bc2d7
Serhiy Yakovlev <ysa.sergey@gmail.com>
2015-11-18 16:31:03 +0200

    Work regular

--- src/FindText.java
+++ src/FindText.java
@@ -22,29 +22,29 @@ public class FindText {
     public void FindREplace(Scanner scannerLg) {
         while (scannerLg.hasNext()) {
             String stTmp = (scannerLg.nextLine());
-            Pattern patern1 = Pattern.compile("\\s\\D*\\.\\s\\D\\."); //Поиск инициалов имени и отчества
+            Pattern patern1 = Pattern.compile("\\s\\D*?\\.\\s\\D\\.\\s"); //Поиск инициалов имени и отчества
             Matcher mat1 = patern1.matcher(stTmp);
-            Pattern patern2 = Pattern.compile("\\s\\D\\.");//Поиск инициалов имени без отчества
+            Pattern patern2 = Pattern.compile("\\s\\D*?\\.\\s");//Поиск инициалов имени без отчества
             Matcher mat2 = patern2.matcher(stTmp);
-            Pattern patern3 = Pattern.compile("\\.\\,\\s\\D+\\s\\D\\.\\s\\D\\.\\s"); //Поиск инициалов имени и отчества 2
+            Pattern patern3 = Pattern.compile("\\.\\,\\s\\D+\\s\\D*?\\.\\s\\D"); //Поиск инициалов имени и отчества 2
             Matcher mat3 = patern3.matcher(stTmp);
 
             if (mat3.find()) {
                 String start = stTmp.substring(0, mat3.end());
                 String end = stTmp.substring(mat3.end());
-                String fin = "Несколько <strong>" + start + "</strong>" + end + "<br/>";
+                String fin = "<strong>" + start + "</strong>" + end + "<br/>";
                 text.add(fin);
 
             } else if (mat1.find()) {
                 String start = stTmp.substring(0, mat1.end());
                 String end = stTmp.substring(mat1.end());
-                String fin = "Имя Инициалы <strong>" + start + "</strong>" + end + "<br/>";
+                String fin = "<strong>" + start + "</strong>" + end + "<br/>";
                 text.add(fin);
 
             } else if (mat2.find()) {
                 String start = stTmp.substring(0, mat2.end());
                 String end = stTmp.substring(mat2.end());
-                String fin = "Одна буква <strong>" + start + "</strong>" + end + "<br/>";
+                String fin = "<strong>" + start + "</strong>" + end + "<br/>";
                 text.add(fin);
 
             } else { //если что то не так
